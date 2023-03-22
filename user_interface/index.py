eb334d85ee6e015bd975149addf215364983fc32
Mohammad Sohal <markvalloway9@gmail.com>
2023-03-23 03:00:55 +0530

    Update index.py

--- user_interface/index.py
+++ user_interface/index.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import pickle
 
-with open('final_data.pkl', 'rb') as file:
+with open('data.pkl', 'rb') as file:
     df = pickle.load(file)
 
 skills = ['PYTHON', 'C++', 'JAVA', 'HADOOP', 'SCALA', 'FLASK',
